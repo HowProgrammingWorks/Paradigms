731c44397d8a5bd296a9081f760fe31da7a19b93
Illia <116695498+Moriartymath@users.noreply.github.com>
2025-06-11 04:04:07 -0700

    Add Factory pattern

--- JavaScript/oop-private.js
+++ JavaScript/oop-private.js
@@ -1,4 +1,17 @@
 'use strict';
+// if we use typescript we could change it to interface or abstract class and then just implement or extends depending on choosed option.
+class MoveStrategy {
+  move(x, y, dx, dy) {
+    throw new Error("Not implemented");
+  }
+}
+// it`s better to implement interface here instead of extending base class
+class LinearMove extends MoveStrategy {
+  move(x, y, dx, dy) {
+    return { x: x + dx, y: y + dy };
+  }
+}
+
 
 class Point {
   #x;
@@ -8,6 +21,15 @@ class Point {
     this.#x = x;
     this.#y = y;
   }
+
+  static createCartesian(x, y) {
+    return new Point(x, y);
+  }
+
+  static createPolar(r, theta) {
+    return new Point(r * Math.cos(theta), r * Math.sin(theta));
+  }
+
 
   move(x, y) {
     this.#x += x;
@@ -30,3 +52,8 @@ console.log(p1.toString());
 const c1 = p1.clone();
 c1.move(-5, 10);
 console.log(c1.toString());
+
+// Creation of instance using static methods
+const p2 = Point.createCartesian(20, 30);
+const p3 = Point.createPolar(5, Math.PI * 2);
+
