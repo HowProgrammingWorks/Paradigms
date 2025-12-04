4abd0fea9d68a9f31b927565b5337d5fab1e43ce
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-12-04 21:18:57 +0200

    Put move to closure too

--- JavaScript/io-pure.js
+++ JavaScript/io-pure.js
@@ -14,8 +14,8 @@ const createMonad = (value) => ({
 
 const createPoint = (x, y) => (fn) => fn(x, y);
 
-const move = (delta) => (point) =>
-  point((x, y) => createPoint(x + delta.x, y + delta.y));
+const move = (dx, dy) => (point) =>
+  point((x, y) => createPoint(x + dx, y + dy));
 const clone = (point) => point((x, y) => createPoint(x, y));
 const toString = (point) => point((x, y) => `(${x}, ${y})`);
 
@@ -27,6 +27,6 @@ const writeLine = (text) => createIO(() => console.log(text));
 const p1 = readPoint().map(createMonad).run();
 p1.map(toString).chain(writeLine).run();
 const c0 = p1.map(clone);
-const t1 = createMonad(move({ x: -5, y: 10 }));
+const t1 = createMonad(move(-5, 10));
 const c1 = t1.ap(c0);
 c1.map(toString).chain(writeLine).run();
